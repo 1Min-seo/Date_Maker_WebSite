3f8e0a47e8655ce9ea0e155fc8361b4944a8a275
woosang <opellong13@gmail.com>
2020-11-23 23:29:16 +0900

    backend

--- app/__init__.py
+++ app/__init__.py
@@ -4,45 +4,24 @@ from flask import (
     request,
     redirect,
     url_for,
-    g,
-    abort,
+    abort, 
     session,
-)
-import pymysql
+    jsonify,
+    make_response,
+    g
+) 
+from flask_cors import CORS 
+import json
+import requests
+from bs4 import BeautifulSoup
+from collections import deque
+import time
+import datetime 
+import re
+import pymysql 
 
-#user 클래스를 만들어 user 객체를 클래스에서 만든 전역변수인 리스트에 append 한다.
-#데이터 베이스를 사용하는 대신에 app 에 global 변수를 사용한다. 
-class User:
-    def __init__(self,id,username,password):
-        self.id = id
-        self.username = username
-        self.password = password
-    
-      
-    def __repr__(self):
-        return f'<User:{self.username}' 
-
-class Set:
-    def __init__(self,cnt,name='로그인'):
-        self.cnt = cnt
-        self.name= name
-    def setName(self,name):
-        self.name = name  
-
-User = Set(0)
-
-#모든 유저들을 나타내는 전역변수 만듬. User 인스턴스들을 append 함
-
-
-# users = [] 
-# users.append(User(id=1,username='우상',password='password1' )) 
-
-
-
-
-print(users[0].password) 
-
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
+CORS(app) 
 app.config.update(
     SECRET_KEY ="woosangyoon1234",
     SESSION_COOKIE_NAME="User_cookie"
@@ -50,87 +29,244 @@ app.config.update(
 
 @app.before_request
 def before_request():
-    g.db = pymysql.connect(host="localhost", port=3306,user='root',passwd='ywoosang',db='user_db',charset='utf8') 
-    #g.user 을 설정. 
-    g.user = None
-    if 'user_id' in session:
-        user_list= [x for x in users if x.id == session['user_id']]
-        if user_list == [] :
-            user = None
-        else :
-            user =user_list[0]
-        g.user = user 
-
-
+    g.db = pymysql.connect(host="localhost", port=3306,user='root',passwd='bodu3717@@',db='user_db',charset='utf8') 
+  
 @app.teardown_request
 def teardown_request(exeption):
     g.db.close()  
 
-@app.route('/login', methods=["GET","POST"])
-def login():
-    #post 로 왔다는 것은 정보를 재전송하고 로그인 버튼을 누른것. 
-    if request.method == 'POST' :
-        session.pop('user_id', None) 
-        #항상 유저가 로그인 시도할 때마다 session 의 user_id 를 pop (제거) 처음부터 다시 시작 
-        # 이미 로그인 되어 있는 한경험이 있는 상태에서 다시 로그인을 시도할 때 , 기존에 있던 세션을 제거하고 새로운 새션을 발급한다.
-        # 로그인을 다시 하려고 할때 비밀번호를 잘못 치면 로그아웃되고 기존에 로그인을 하고있더라도 다시 정확한 비밀번호를 입력해야한다. 
-        user_id = request.form.get('Id')
-        user_pw = request.form.get('passwd')
+class Crawl:
+    nameList =[]
+    numList =[]
+    addList =[]
+    @classmethod
+    def getText(cls,tag):
+        return tag.get_text().strip('+')      
+    @classmethod
+    def coronaData(cls):
+        res = requests.get("https://www.seoul.go.kr/coronaV/coronaStatus.do")
+        soup = BeautifulSoup(res.content,"lxml")
+        getdata =soup.select("table.tstyle-status.pc.pc-table > tbody")[0] 
+        getName = getdata.findAll('th')[:-1] 
+        getAdd= getdata.findAll('td',class_="today")[:-1]
+        getNum = getdata.findAll('td',class_='')[:-1]
+        nameList= list(map(Crawl.getText,getName))
+        numList= list(map(Crawl.getText,getNum))
+        addList= list(map(Crawl.getText,getAdd))
+        return [nameList,numList,addList] #2차원 배열로 만들어준다
+    @classmethod
+    def seoulData(cls):
+        res = requests.get("http://ncov.mohw.go.kr/")
+        soup = BeautifulSoup(res.content,"lxml")
+        getdata = soup.select("#map_city1 > div > ul")[0]
+        num =getdata.findAll('span',class_ = "num")
+        sub_num =getdata.find('span',class_ = "sub_num")
+        sub_num=sub_num.string.strip('()')
+        numList = deque(list(map(Crawl.getText,num)))
+        numList.appendleft(sub_num)
+        return list(numList)
 
-        sql = """select id from user_table where id = '%s' """ % user_id
+class Restaurant:
+    @classmethod
+    def getRestaurant(cls):
+        cursor = g.db.cursor()
+        sql ="""select * from date_restaurant"""
         cursor.execute(sql)
-        select_id = cursor.fetchone()
-        sql = """SELECT pwd from user_table where pwd = '%s' """ % user_pw
+        data = list(cursor.fetchall()) #튜플 => 리스트
+        return data 
+
+class User :
+    def __init__(self,user_id,user_name,user_password):
+        self.id = user_id
+        self.name =user_name
+        self.passwd = user_password
+    def get_id(self):
+        return self.id 
+    @staticmethod 
+    def find(user_id):  # 사용자가 존재하는지 확인
+        cursor = g.db.cursor()
+        sql = """select * from user_table where user_id='%s'""" % str(user_id) 
         cursor.execute(sql)
-        select_pwd = cursor.fetchone()
-        #오류 방지를 위해 strip 으로 가능한 경우 모두제거 
-        db_id = str(select_id).strip(" ,('')")  
-        db_pw = str(select_pwd).strip(" ,('')")
-        
-        if user_id == db_id and user_pw == db_pw :
-            g.id = db_id
-            g.pw = db_pw
-            #아이디 비번이 일치할 경우 메인페이지로 이동 
-            sql = """SELECT user_name from user_table where pwd = '%s' """ % user_pw
-            cursor.execute(sql)
-            #이름 오류 있을시 확인 요망
-            raw_name= cursor.fetchone()
-            name = str(raw_name).strip(" ,('')")
-            User.setName(name) 
-            #key : user_id  세션 설정 
-            # 여기있는 세션은 cookie 로 내려보내짐. 
-            session['user_id'] = user.id
-            #메인 페이지로 이동시킴
-            return redirect(url_for('mainPage',name="%s" %  str(User.name)))
+        user = cursor.fetchone() #튜플 형식 (아이디,이름,비밀번호)
+        if user == None: #없으면 None
+            return None
+        user_id=user[0]
+        user_name=user[1]
+        user_passwd=user[2]
+        user = User(user_id,user_name,user_passwd)  #인스턴스 생성
+        return user #User 인스턴스가 반환 
+
+    @staticmethod
+    def findProfile(user_id):  #프로필이 있는지 없는지 Boolean 으로 
+        cursor = g.db.cursor()
+        sql = """select * from date_table where user_id='%s'""" % str(user_id) 
+        cursor.execute(sql) 
+        profile = cursor.fetchone() 
+        if profile :
+            return False #있으면 false 
         else:
-            #일치하지 않을 경우 실패 횟수를 증가시키고 다시 로그인 페이지로 이동
-            User.cnt +=1
-            print(User.cnt)
-            #실패 횟수가 3번 이상일 경우 없는 회원으로 간주하고 가입페이지로 이동 
-            if User.cnt >= 3 :
-               User.cnt = 0 
-               return redirect(url_for('signup'))
-            return redirect(url_for('login',alert="로그인오류 "+str(User.cnt)+"회") )
-        return render_template('login.html',alert=alert)
+            return True #없으면 True 
 
+    @staticmethod
+    def makeUser(user_id,user_name,user_passwd):  #회원 가입
+        isUser = User.find(user_id) #사용자가 있나 검사 
+        if isUser == None :
+            cursor = g.db.cursor()
+            sql = """insert into user_table (user_id,user_name,user_passwd) values ('%s','%s','%s')""" % (str(user_id),str(user_name),str(user_passwd)) 
+            cursor.execute(sql)
+            g.db.commit() 
+            return User.find(user_id) #User 인스턴스가 반환
+        else  :
+            return isUser #User 인스턴스가 반환 
 
+    @staticmethod
+    def makeProfile(user_id,start_date): #start date = [시작년도,시작월,시작 날] 프론트엔드에서 받아서 입력해주면 백엔드에서 다시 쏴주기 
+        userId = user_id  
+        isProfile = User.findProfile(userId)
+        if isProfile == False :
+            return None  # 이미 프로필이 생성된 유저 
+        cursor = g.db.cursor() 
+        now = time.localtime() 
+        year = start_date[0]
+        month = start_date[1]
+        day = start_date[2]
+        meetDay = "%d/%d/%d" % (year,month,day)  #만난 날짜 입력받게 해서 백엔드로 가져온다.
+        today = "%04d/%02d/%02d" % (now.tm_year, now.tm_mon, now.tm_mday) #오늘 데이트 했으면 체크할 수 있도록 한다.
+        dayArray= [] #초기 설정 
+        json_dayArray = json.dumps(dayArray,ensure_ascii=False)  
+        sql ="""insert into date_table values('%s','%s','%s') """ % (str(meetDay),str(userId),json_dayArray)
+        cursor.execute(sql) 
+        g.db.commit() 
+        return True # 프로필이 잘 생성된 경우 
 
-@app.route('/profile')   
-def profile():
-    if not g.user:
-        # 세션이 없는 유저라면 forbidden error 내고 로그인 페이지로 이동시키기
-        abort(403) 
-        return render_template('login.html')
-    return render_template('profile.html')   
+    @staticmethod
+    def getDate(user_id):
+        cursor = g.db.cursor()
+        try:
+            sql ="""select meet_day from date_table where user_id='%s'""" % str(user_id) 
+            cursor.execute(sql)
+        except Exception as e: # 에러 종류
+            print('에러가 발생 했습니다', e)
+        date = list(map(int,cursor.fetchone()[0].split('/')))
+        meetday = datetime.datetime(date[0],date[1],date[2]) 
+        today = datetime.datetime.now() 
+        ingday = re.findall('\d+',str(today-meetday))[0]  
+        return int(ingday)+1 #사귄 날부터 1일 설정 
 
+    @staticmethod
+    def addDateday(user_id,day):
+        cursor = g.db.cursor()
+        userId = user_id
+        addday = day
+        try :
+            sql = """select date_day from date_table where user_id='%s'""" % str(userId)
+            cursor.execute(sql)
+            dayArray = eval(cursor.fetchone()[0]) 
+        except Exception as e: # 에러 종류
+            print('에러가 발생 했습니다', e)
+        if addday not in dayArray: 
+            dayArray.append(addday)
+            json_dayArray = json.dumps(dayArray,ensure_ascii=False)
+            sql ="""UPDATE date_table SET date_day='%s' where user_id='%s'""" %  (json_dayArray,str(userId)) 
+            cursor.execute(sql) 
+            g.db.commit() 
 
-app.run(host='0.0.0.0',debug=True)
-
-#유저가 로그인폼에 입력하면 app이 username과 password 가 맞는지 검사하고 
-# 맞다면 app 은 session 을 이용해 user 가 로그인했는지, 로그인하지 않았는지 log 를 적고
-# 프로필 페이지로 redirect 시킨다. 
-
-
+    @staticmethod
+    def delDateday(user_id,day):
+        cursor = g.db.cursor()
+        delday = day
+        try :
+            sql = """select date_day from date_table where user_id='%s'""" % str(user_id)
+            cursor.execute(sql)
+        except Exception as e: # 에러 종류
+            print('에러가 발생 했습니다', e)
+        dayArray = eval(cursor.fetchone()[0])
+        if delday in dayArray: 
+            dayArray.remove(delday)
+            json_dayArray = json.dumps(dayArray,ensure_ascii=False)
+            sql ="""UPDATE date_table SET date_day='%s' where user_id='%s'""" %  (json_dayArray,str(user_id)) 
+            cursor.execute(sql) 
+            g.db.commit() 
 
  
+        
+@app.route('/login',methods=["GET","POST"])
+def login(): 
+    if request.method == "POST":
+        cursor = g.db.cursor()
+        user_id = request.form.get('Id')
+        user_pw = request.form.get('passwd') 
+        # name.user = user_id
+        try :
+            sql = """select id from user_table where id = '%s' """ % user_id
+            cursor.execute(sql)
+            select_id = cursor.fetchone()
+            sql = """SELECT pwd from user_table where pwd = '%s' """ % user_pw
+            cursor.execute(sql)
+            select_pwd = cursor.fetchone()
+            db_id = str(select_id).strip(" ,('')")
+            db_pw = str(select_pwd).strip(" ,('')")
+            print(db_id)
+            print(db_pw) 
 
+        except Exception as e :
+            print("예외 발생.", e)
+            return redirect(url_for('login',alert="예외 발생")) 
+
+        if user_id == db_id and user_pw == db_pw : 
+            #아이디 비번이 일치할 경우 메인페이지로 이동 
+            return redirect(url_for('main'))
+        else:
+            makeAlert ="<script>alert('로그인 오류 !')</script>"  
+            return render_template("login.html",makeAlert=makeAlert)  
+    return render_template('login.html',makeAlert='')
+
+
+@app.route('/signup',methods=["GET","POST"])
+def signup(): 
+    if request.method == "POST":
+        cursor = g.db.cursor()
+        name = request.form.get('name')
+        user_id = request.form.get('Id')
+        user_pw = request.form.get('passwd')
+        print(name)
+        print(user_id)
+        print(user_pw)
+        # primary key를 id 로 바꿔야 중복이 안됌.
+        try : 
+            sql ="""insert into user_table values('%s','%s','%s');""" % (str(name),str(user_id),str(user_pw))
+            cursor.execute(sql)
+        except Exception as e :
+            print("예외 발생.", e)
+            return redirect(url_for('signup'))  
+        sql ="""select * from user_table ;"""
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+        g.db.commit()
+        #회원가입에 성공한 후 로그인 페이지로 이동 
+        return  redirect(url_for('login')) 
+    return render_template("sign-up.html")
+
+@app.route('/datemaker/corona')
+def coronaPage():
+    return render_template("public/corona.html") 
+     
+@app.route('/corona',methods=["POST","GET"])
+def coronaRes():
+    response = Crawl.coronaData() 
+    print(response)
+    return make_response(jsonify(response),200)
+
+@app.route('/')
+def gomain():
+    return  redirect(url_for('main'))
+@app.route('/datemaker')
+def main():
+    return render_template('public/index.html') 
+
+
+
+if __name__ == "__main__" :
+    app.run(debug=True)
+    
